5433c97046b51165aed127f921da8a35d5da068d
Lokathor <zefria@gmail.com>
2022-07-17 05:10:19 +0000

    Deploying to gh-pages from @ Lokathor/arm7tdmi_mem_fns@e6d5745905dae8a4ec81b591a1a54c313ca81b28 🚀

--- search-index.js
+++ search-index.js
@@ -1,5 +1,5 @@
 var searchIndex = JSON.parse('{\
-"arm7tdmi_mem_fns":{"doc":"","t":[5,5,5],"n":["copy_u8_forward","copy_u8_overlapping","copy_u8_reverse"],"q":["arm7tdmi_mem_fns","",""],"d":["copies from low to high address.","copies the memory, automatically determines the correct …","copies from high to low address"],"i":[0,0,0],"f":[0,0,0],"p":[]}\
+"arm7tdmi_mem_fns":{"doc":"","t":[5,5,5,5,5,5],"n":["copy_u16_forward","copy_u16_overlapping","copy_u16_reverse","copy_u8_forward","copy_u8_overlapping","copy_u8_reverse"],"q":["arm7tdmi_mem_fns","","","","",""],"d":["copies from low to high address.","copies the memory, automatically determines the correct …","copies from high to low address","copies from low to high address.","copies the memory, automatically determines the correct …","copies from high to low address"],"i":[0,0,0,0,0,0],"f":[0,0,0,0,0,0],"p":[]}\
 }');
 if (typeof window !== 'undefined' && window.initSearch) {window.initSearch(searchIndex)};
 if (typeof exports !== 'undefined') {exports.searchIndex = searchIndex};
