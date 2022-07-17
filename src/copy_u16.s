e6d5745905dae8a4ec81b591a1a54c313ca81b28
Lokathor <zefria@gmail.com>
2022-07-16 23:09:49 -0600

    copy u16

---
+++ src/copy_u16.s
@@ -0,0 +1,35 @@
+
+.global copy_u16_overlapping
+.global copy_u16_forward
+.global copy_u16_reverse
+
+copy_u16_overlapping:
+    cmp     r0, r1
+    bgt     copy_u16_reverse
+    @ fallthough to copy_u16_forward
+
+copy_u16_forward:
+    subs    r2, r2, #2
+    ldrhcs  r3, [r1], #2
+    strhcs  r3, [r0], #2
+    bgt     copy_u16_forward
+    bxeq    lr
+    adds    r2, r2, #1
+    ldrbeq  r3, [r1]
+    strbeq  r3, [r0]
+    bx      lr
+
+copy_u16_reverse:
+    tst     r2, #1
+    bne     2f
+  1:
+    subs    r2, r2, #2
+    ldrhcs  r3, [r1, r2]
+    strhcs  r3, [r0, r2]
+    bgt     1b
+    bxeq    lr
+  2:
+    sub     r2, r2, #1
+    ldrb    r3, [r1]
+    strb    r3, [r0]
+    b       1b
--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,6 @@
 mod copy_u8;
 pub use copy_u8::*;
 
+mod copy_u16;
+pub use copy_u16::*;
+
