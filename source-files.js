a6202331fefee54a24200f67e5125465e2ef6151
Lokathor <zefria@gmail.com>
2022-07-17 04:54:12 +0000

    Deploying to gh-pages from @ Lokathor/arm7tdmi_mem_fns@520e26a9f1e5776f1eb0917c291120a3354f8976 🚀

--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var sourcesIndex = {};
-sourcesIndex["arm7tdmi_mem_fns"] = {"name":"","files":["lib.rs"]};
+sourcesIndex["arm7tdmi_mem_fns"] = {"name":"","files":["copy_u8.rs","lib.rs"]};
 createSourceSidebar();
