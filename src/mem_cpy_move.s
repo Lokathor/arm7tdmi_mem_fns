2f9e240f166ad1e67f5eb714f64e44862022499e
Lokathor <zefria@gmail.com>
2022-07-19 22:54:45 -0600

    clean up to simple versions with strong test suite

--- src/mem_cpy_move.s
+++ src/mem_cpy_move.s
@@ -1,212 +1,99 @@
 
-@ function names deliberately avoid the "actual" names to work with the test suite.
-@ when using this code in a project, remove the leading `z` with search and replace.
+.global aeabi_memmove1
+.global aeabi_memcpy1
+.global aeabi_memmove2
+.global aeabi_memcpy2
+.global aeabi_memmove4
+.global aeabi_memcpy4
+.global aeabi_memmove8
+.global aeabi_memcpy8
 
-.global zmemcpy
-.global z__aeabi_memcpy
-.global z__aeabi_memcpy4
-.global z__aeabi_memcpy8
+@ the pointers are max aligned to 1
 
-.global z__aeabi_memcpy_vram
-.global z__aeabi_memcpy_sram
-
-.global zmemmove
-.global z__aeabi_memmove
-.global z__aeabi_memmove4
-.global z__aeabi_memmove8
-
-@
-@ MOVE
-@
-
-zmemmove:
-    push   {r0, lr}
-    bl     z__aeabi_memmove
-    pop    {r0, lr}
-    bx     lr
-
-z__aeabi_memmove4:
-z__aeabi_memmove8:
+aeabi_memmove1:
     cmp    r0, r1
-    bgt    .L_aeabi_memcpy_reverse
-    b      z__aeabi_memcpy4
-
-@ Note: unaligned aeabi memmove is in the forward copy code area for fallthrough
-
-@
-@ COPY FORWARD
-@
-
-zmemcpy:
-    push    {r0, lr}
-    bl      z__aeabi_memcpy
-    pop     {r0, lr}
-    bx      lr
-
-z__aeabi_memmove:
-    cmp     r0, r1
-    bgt     .L_aeabi_memcpy_reverse
-    @ fallthrough
-z__aeabi_memcpy:
-    eor     r12, r0, r1
-    lsls    r12, r12, #31
-    @ max align 1
-    bmi     z__aeabi_memcpy_sram
-    @ max align 2
-    bcs     .L_align2_forward_fixup
-    @ max align 4
-    tst     r0, #3
-    bne     .L_align4_forward_fixup
-    @ fallthrough
-z__aeabi_memcpy8:
-z__aeabi_memcpy4:
-    cmp     r2, #32
-    bge     .L_block_forward_entry
-  .L_block_forward_after:
-    @ check for 4 word copy
-    subs    r2, r2, #16
-    ldmcs   r1!, {r3, r12}
-    stmcs   r0!, {r3, r12}
-    ldmcs   r1!, {r3, r12}
-    stmcs   r0!, {r3, r12}
-    bxeq    lr @ early return
-    @ carry/sign for 2 word and/or 1 word copy
-    lsls    r3, r2, #29
-    ldmcs   r1!, {r3, r12}
-    stmcs   r0!, {r3, r12}
-    ldrmi   r3, [r1], #4
-    strmi   r3, [r0], #4
-    @ carry/sign for halfword and/or byte copy
-    lsls    r3, r2, #31
-    ldrhcs  r3, [r1], #2
-    strhcs  r3, [r0], #2
-    ldrbmi  r3, [r1], #1
-    ldrbmi  r3, [r0], #1
-    bx      lr
-  .L_block_forward_entry:
-    push    {r4-r9}
-  1:
-    subs    r2, r2, #32
-    ldmcs   r1!, {r3-r9, r12}
-    stmcs   r0!, {r3-r9, r12}
-    bgt     1b
-    pop     {r4-r9}
-    bxeq    lr @ early return
-    adds    r2, r2, #32
-    b       .L_block_forward_after
-  .L_align4_forward_fixup:
-    lsls    r12, r0, #31
-    @ if current align 1, advance by 1
-    submi   r2, r2, #1
-    ldrbmi  r3, [r1], #1
-    ldrbmi  r3, [r0], #1
-    @ if current align 2, advance by 2
-    subcs   r2, r2, #2
-    ldrhcs  r3, [r1], #2
-    ldrhcs  r3, [r0], #2
-    b       z__aeabi_memcpy4
-
-  .L_align2_forward_fixup:
-    tst     r0, #1
-    ldrbne  r3, [r1], #1
-    ldrbne  r3, [r0], #1
-z__aeabi_memcpy_vram:
-    subs    r2, r2, #2
-    ldrhcs  r3, [r1], #2
-    strhcs  r3, [r0], #2
-    bgt     z__aeabi_memcpy_vram
-    bxeq    lr
-    adds    r2, r2, #1
-    ldrbeq  r3, [r1]
-    strbeq  r3, [r0]
-    bx      lr
-
-z__aeabi_memcpy_sram:
+    bgt    .L_r_copy_u8
+aeabi_memcpy1:
+  .L_f_copy_u8:
     subs    r2, r2, #1
     ldrbcs  r3, [r1], #1
     strbcs  r3, [r0], #1
-    bgt     z__aeabi_memcpy_sram
+    bgt     .L_f_copy_u8
+    bx      lr
+  .L_r_copy_u8:
+    subs    r2, r2, #1
+    ldrbcs  r3, [r1, r2]
+    strbcs  r3, [r0, r2]
+    bgt     .L_r_copy_u8
     bx      lr
 
-@
-@ COPY REVERSE
-@
+@ the pointers are max aligned to 2
 
-  .L_aeabi_memcpy_reverse:
-    eor    r12, r0, r1
-    lsls   r12, r12, #31
-    @ max align 1
-    bmi    .L_memcpy_sram_reverse
-    @ max align 2
-    bcs    .L_align2_reverse_fixup
-    @ max align 4
-    /* If we have to reverse copy, and we're unaligned, rip */
-    tst    r0, #3
-    bne    .L_memcpy_sram_reverse
-    @ fallthrough
-  .L__aeabi_memcpy4_reverse:
-    tst     r2, #3
-    bne     .L_align4r_handle_byte_and_halfword
-  .L_align4r_count_is_now_a_multiple_of_four:
-    add     r1, r1, r2
-    add     r0, r0, r2
-    tst     r2, #32
-    bge     .L_align4r_block_copy_sub
-  .L_align4r_done_with_block_copy:
-    tst     r2, #(1<<4)
-    ldmdbne r1!, {r3, r12}
-    stmdbne r0!, {r3, r12}
-    ldmdbne r1!, {r3, r12}
-    stmdbne r0!, {r3, r12}
-    lsls    r3, r2, #29
-    ldmdbcs r1!, {r3, r12}
-    stmdbcs r0!, {r3, r12}
-    ldrmi   r3, [r1, #-4]
-    strmi   r3, [r0, #-4]
-    bx      lr
-  .L_align4r_block_copy_sub:
-    push    {r4-r9}
-  1:
-    subs    r2, r2, #32
-    ldmdbcs r1!, {r3-r9, r12}
-    stmdbcs r0!, {r3-r9, r12}
-    bgt     1b
-    pop     {r4-r9}
+aeabi_memmove2: @ this is mostly exposed for testing purposes
+    cmp     r0, r1
+    bgt     .L_r_copy_u16
+aeabi_memcpy2:
+  .L_f_copy_u16:
+    subs    r2, r2, #2
+    ldrhcs  r3, [r1], #2
+    strhcs  r3, [r0], #2
+    bgt     .L_f_copy_u16
     bxeq    lr
-    adds    r2, r2, #32
-    b       .L_align4r_done_with_block_copy
-  .L_align4r_handle_byte_and_halfword:
-    lsls    r3, r2, #31
-    submi   r2, r2, #1
-    ldrbmi  r3, [r1, r2]
-    strbmi  r3, [r0, r2]
-    subcs   r2, r2, #2
+    tst     r2, #1
+    ldrbne  r3, [r1]
+    strbne  r3, [r0]
+    bx      lr
+  .L_r_copy_u16:
+    tst     r2, #1
+    bne     2f
+  1:
+    subs    r2, r2, #2
     ldrhcs  r3, [r1, r2]
     strhcs  r3, [r0, r2]
-    b       .L_align4r_count_is_now_a_multiple_of_four
+    bgt     1b
+    bx      lr
+  2:
+    sub     r2, r2, #1
+    ldrb    r3, [r1, r2]
+    strb    r3, [r0, r2]
+    b       1b
 
-  .L_align2_reverse_fixup:
-    /* If we have to reverse copy and we're unaligned, rip */
-    tst    r0, #1
-    bne    .L_memcpy_sram_reverse
-  .L_memcpy_vram_reverse:
-    tst    r2, #1
-    bne    2f @ handle tail byte, if any
+@ the pointers are max aligned to 4
+
+aeabi_memmove8:
+aeabi_memmove4:
+    cmp    r0, r1
+    bgt    .L_r_copy_u32
+aeabi_memcpy8:
+aeabi_memcpy4:
+  .L_f_copy_u32:
+    subs   r2, r2, #4
+    ldrcs  r3, [r1], #4
+    strcs  r3, [r0], #4
+    bgt    .L_f_copy_u32
+    bxeq   lr
+    lsls   r3, r2, #31
+    ldrhcs r3, [r1], #2
+    strhcs r3, [r0], #2
+    ldrbmi r3, [r1], #1
+    strbmi r3, [r0], #1
+    bx     lr
+  .L_r_copy_u32:
+    tst    r2, #3
+    bne    2f
   1:
-    subs   r2, r2, #2
-    ldrhcs r3, [r1, r2]
-    strhcs r3, [r0, r2]
+    subs   r2, r2, #4
+    ldrcs  r3, [r1, r2]
+    strcs  r3, [r0, r2]
     bgt    1b
     bx     lr
   2:
-    sub    r2, r2, #1
-    ldrb   r2, [r1, r2]
-    strb   r2, [r0, r2]
+    lsls   r3, r2, #31
+    submi  r2, r2, #1
+    ldrbmi r3, [r1, r2]
+    strbmi r3, [r0, r2]
+    subcs  r2, r2, #2
+    ldrhcs r3, [r1, r2]
+    strhcs r3, [r0, r2]
     b      1b
 
-  .L_memcpy_sram_reverse:
-    subs   r2, r2, #1
-    ldrbcs r3, [r1, r2]
-    strbcs r3, [r0, r2]
-    bgt    .L_memcpy_sram_reverse
-    bx     lr
--- tests/test_mem_cpy_move.rs
+++ tests/test_mem_cpy_move.rs
@@ -1,9 +1,50 @@
+#![allow(dead_code)]
 
-use arm7tdmi_mem_fns::*;
+core::arch::global_asm!(include_str!("../src/mem_cpy_move.s"), options(raw));
+
+extern "C" {
+  /// Copies `d` to `s`.
+  /// * The two regions **must not** overlap.
+  /// * The maximum co-alignment is assumed to be 1.
+  fn aeabi_memcpy1(d: *mut u8, s: *const u8, bytes: usize);
+
+  /// Copies to `d` from `s`.
+  /// * The two regions *may* overlap.
+  /// * The maximum co-alignment is assumed to be 1.
+  fn aeabi_memmove1(d: *mut u8, s: *const u8, bytes: usize);
+
+  /// Copies to `d` from `s`.
+  /// * The two regions **must not** overlap.
+  /// * The two pointers **must** be aligned to 2.
+  /// * The maximum co-alignment is assumed to be 2.
+  fn aeabi_memcpy2(d: *mut u16, s: *const u16, bytes: usize);
+
+  /// Copies to `d` from `s`.
+  /// * The two regions *may* overlap.
+  /// * The two pointers **must** be aligned to 2.
+  /// * The maximum co-alignment is assumed to be 2.
+  fn aeabi_memmove2(d: *mut u16, s: *const u16, bytes: usize);
+
+  /// Copies to `d` from `s`.
+  /// * The two regions **must not** overlap.
+  /// * The two pointers **must** be aligned to 4.
+  fn aeabi_memcpy4(d: *mut u32, s: *const u32, bytes: usize);
+
+  /// Copies to `d` from `s`.
+  /// * The two regions *may* overlap.
+  /// * The two pointers **must** be aligned to 4.
+  fn aeabi_memmove4(d: *mut u32, s: *const u32, bytes: usize);
+}
 
 fn rand_bytes(n: usize) -> Vec<u8> {
   let mut v = vec![0; n];
   getrandom::getrandom(&mut v).unwrap();
+  v
+}
+
+fn rand_halfwords(n: usize) -> Vec<u16> {
+  let mut v = vec![0_u16; n];
+  getrandom::getrandom(bytemuck::cast_slice_mut(&mut v)).unwrap();
   v
 }
 
@@ -19,61 +60,70 @@ fn rand_u32() -> u32 {
   u32::from_ne_bytes(bytes)
 }
 
-struct BadLCG(u32);
-impl BadLCG {
+struct Lcg(u32);
+impl Lcg {
+  fn new() -> Self {
+    Self(rand_u32())
+  }
   fn next_u32(&mut self) -> u32 {
-    self.0 = self.0.wrapping_mul(5).wrapping_add(1);
+    self.0 = self.0.wrapping_mul(747796405).wrapping_add(1);
     self.0
   }
 }
 
-fn do_move(mut b: Vec<u8>, d: usize, s: usize, count: usize) {
-  let mut clone = b.clone();
-  clone.copy_within(s..(s+count), d);
-  unsafe {
-    let p = b.as_mut_ptr();
-    zmemmove(p.add(d), p.add(s), count);
-  }
-  assert_eq!(clone, b, "failure: d:{d}, s:{s}, count:{count}");
-}
-
 #[test]
-fn test_zmemmove() {
-  let mut lcg = BadLCG(rand_u32());
-  
+fn test_aeabi_memmove1(){
+  let mut lcg = Lcg::new();
   for _ in 0..10 {
-    for count in 0..138 {
-      let b = rand_bytes(1024);
-      let d = (lcg.next_u32() % 512) as usize;
-      let s = (lcg.next_u32() % 512) as usize;
-      do_move(b, d, s, count);
+    for bytes in 0..99 {
+      let mut buffer = rand_bytes(256);
+      let mut clone = buffer.clone();
+      let d = (lcg.next_u32() % 128) as usize;
+      let s = (lcg.next_u32() % 128) as usize;
+      unsafe {
+        let p = buffer.as_mut_ptr();
+        aeabi_memmove1(p.add(d), p.add(s), bytes);
+      }
+      clone.copy_within(s..(s+bytes), d);
+      assert_eq!(clone, buffer, "\nd: {d},\ns: {s},\nbytes: {bytes}");
     }
   }
 }
 
-fn do_move4(mut b: Vec<u32>, d: usize, s: usize, count: usize) {
-  let mut clone = b.clone();
-  clone.copy_within(s..(s+count), d);
-  unsafe {
-    let p = b.as_mut_ptr();
-    z__aeabi_memmove4(p.add(d).cast(), p.add(s).cast(), count);
+#[test]
+fn test_aeabi_memmove2() {
+  let mut lcg = Lcg::new();
+  for _ in 0 .. 10 {
+    for bytes in 0..99 {
+      let mut buffer = rand_halfwords(256);
+      let mut clone = buffer.clone();
+      let d = (lcg.next_u32() % 128) as usize;
+      let s = (lcg.next_u32() % 128) as usize;
+      unsafe {
+        let p: *mut u16 = buffer.as_mut_ptr();
+        aeabi_memmove2(p.add(d), p.add(s), bytes);
+      }
+      bytemuck::cast_slice_mut::<u16, u8>(&mut clone).copy_within((s*2)..(s*2+bytes), d*2);
+      assert_eq!(clone, buffer, "\nd: {d},\ns: {s},\nbytes: {bytes}");
+    }
   }
-  assert_eq!(clone, b, "\nfailure:\nd:{d}\ns:{s}\ncount:{count}");
 }
 
 #[test]
-fn test_z__aeabimemmove4() {
-  let mut lcg = BadLCG(rand_u32());
-
-  do_move4(rand_words(2), 0, 1, 1);
-  do_move4(rand_words(2), 1, 0, 1);
-
+fn test_aeabi_memmove4() {
+  let mut lcg = Lcg::new();
   for _ in 0 .. 10 {
-    for words in 0..9 {
-      let b = rand_words(16);
-      let d = (lcg.next_u32() % 4) as usize;
-      let s = (lcg.next_u32() % 4) as usize;
-      do_move4(b, d, s, words);
+    for bytes in 0 .. 128 {
+      let mut buffer = rand_words(256);
+      let mut clone = buffer.clone();
+      let d = (lcg.next_u32() % 128) as usize;
+      let s = (lcg.next_u32() % 128) as usize;
+      unsafe {
+        let p: *mut u32 = buffer.as_mut_ptr();
+        aeabi_memmove4(p.add(d), p.add(s), bytes);
+      }
+      bytemuck::cast_slice_mut::<u32, u8>(&mut clone).copy_within((s*4)..(s*4+bytes),d*4);
+      assert_eq!(clone, buffer, "\nd: {d},\ns: {s},\nbytes: {bytes}");
     }
   }
 }
