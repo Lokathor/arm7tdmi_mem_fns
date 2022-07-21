0157f5437aaf45caf1aa22f6e5f8c427478fd599
Lokathor <zefria@gmail.com>
2022-07-21 17:41:29 -0600

    Update mem_cpy_move.s

--- src/mem_cpy_move.s
+++ src/mem_cpy_move.s
@@ -1,5 +1,3 @@
-.global libc_memmove
-
 libc_memmove:
     push   {r0, lr}
     bl     aeabi_memmove
@@ -7,22 +5,56 @@ libc_memmove:
     bx     lr
 
 aeabi_memmove:
-    cmp    r0, r1 @ if d < s, reverse copy
-    bgt    .L_r_copy
-  .L_f_copy:
+    cmp    r0, r1 @ if d > s, reverse copy
+    bgt    .L_r_copy_gain_align
+aeabi_memcpy:
+  .L_f_copy_gain_align:
+    eor    r12, r0, r1
+    lsls   r3, r12, #31
+    bmi    .L_f_copy_max_coalign1
+    bcs    .L_f_copy_max_coalign2
+    @ else fallthrough
+
+  .L_f_copy_max_coalign4:
+    @ todo
+  .L_f_copy_coalign4_assured:
+    @ todo
+  .L_f_copy_fixup4:
+    @ todo
+
+  .L_f_copy_max_coalign2:
+    tst     r0, #1
+    bne     .L_f_copy_fixup2
+  .L_f_copy_coalign2_assured:
+    @ todo
+  .L_f_copy_fixup2:
+    @ todo
+    b       .L_f_copy_coalign2_assured
+
+  .L_f_copy_max_coalign1:
   1:
-    subs   r2, r2, #1
-    ldrbge r3, [r1], #1
-    strbge r3, [r0], #1
-    bgt    1b
-    bx     lr
-  .L_r_copy:
-    add r0, r0, r2
-    add r1, r1, r2
-  .L_r_copy_post_ptr_fix:
+    subs    r2, r2, #1
+    ldrbge  r3, [r1], #1
+    strbge  r3, [r0], #1
+    bgt     1b
+    bx      lr
+
+  .L_r_copy_gain_align:
+    add     r0, r0, r2
+    add     r1, r1, r2
+    eor     r12, r0, r1
+    lsls    r3, r12, #31
+    bmi     .L_r_copy_max_coalign1
+    bcs     .L_r_copy_max_coalign2
+    @ else fallthrough
+  .L_r_copy_max_coalign4:
+    @ todo
+  .L_r_copy_max_coalign2:
+    @ todo
+  .L_r_copy_max_coalign1:
   1:
-    subs   r2, r2, #1
-    ldrbge r3, [r1, #-1]!
-    strbge r3, [r0, #-1]!
-    bgt    1b
-    bx     lr
+    subs    r2, r2, #1
+    ldrbge  r3, [r1, #-1]!
+    strbge  r3, [r0, #-1]!
+    bgt     1b
+    bx      lr
