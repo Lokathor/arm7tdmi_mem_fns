34662869c3d95d9220fce3cc96e882e96caf224b
Lokathor <zefria@gmail.com>
2022-07-23 12:57:41 -0600

    Update unaligned.s

--- src/unaligned.s
+++ src/unaligned.s
@@ -4,14 +4,25 @@
 .global aeabi_uread8
 .global aeabi_uwrite8
 
-.section ".text.aeabi.uread4"
+.section ".text.aeabi.uread"
+aeabi_uread8:
+    ldrb r1, [r0, #4]
+    ldrb r2, [r0, #5]
+    orr  r1, r1, r2, lsl #8
+    ldrb r2, [r0, #6]
+    orr  r1, r1, r2, lsl #16
+    ldrb r2, [r0, #7]
+    orr  r1, r1, r2, lsl #24
+    @ fallthrough
 aeabi_uread4:
-    ldrb r1, [r0], #1
-    ldrb r2, [r0], #1
-    ldrb r3, [r0], #1
-    ldrb r12, [r0], #1
-    orr  r0, r1, r2, lsl #8
-    orr  r0, r0, r3, lsl #16
-    orr  r0, r0, r12, lsl #24
+    @ r1 may already hold output data!
+    ldrb r2, [r0]
+    ldrb r3, [r0, #1]
+    orr  r2, r2, r3, lsl #8
+    ldrb r3, [r0, #2]
+    orr  r2, r2, r3, lsl #16
+    ldrb r3, [r0, #3]
+    orr  r2, r2, r3, lsl #24
+    mov  r0, r2
     bx   lr
 .previous
