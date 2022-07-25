0687dd76259a8271a5f8b0951aa19e9b041def7c
Lokathor <zefria@gmail.com>
2022-07-25 11:37:54 -0600

    Update and rename the_code.s to a32_assembly.s

--- src/a32_assembly.s
+++ src/a32_assembly.s
@@ -321,63 +321,6 @@ aeabi_memset: @ r0(dest), r1(count), r2(byte)
     bx     lr
 .previous
 
-.section ".text.aeabi.unaligned.read8"
-aeabi_uread8:
-    ldrb r1, [r0, #4]
-    ldrb r2, [r0, #5]
-    orr  r1, r1, r2, lsl #8
-    ldrb r2, [r0, #6]
-    orr  r1, r1, r2, lsl #16
-    ldrb r2, [r0, #7]
-    orr  r1, r1, r2, lsl #24
-    b    aeabi_uread4
-.previous
-
-.section ".text.aeabi.unaligned.read4"
-aeabi_uread4:
-    @ r1 may already hold output data!
-    ldrb r2, [r0]
-    ldrb r3, [r0, #1]
-    orr  r2, r2, r3, lsl #8
-    ldrb r3, [r0, #2]
-    orr  r2, r2, r3, lsl #16
-    ldrb r3, [r0, #3]
-    orr  r2, r2, r3, lsl #24
-    mov  r0, r2
-    bx   lr
-.previous
-
-.section ".text.aeabi.unaligned.write8"
-aeabi_uwrite8:
-    strb r0, [r2]
-    lsr  r3, r0, #8
-    strb r3, [r2, #1]
-    lsr  r3, r3, #8
-    strb r3, [r2, #2]
-    lsr  r3, r3, #8
-    strb r3, [r2, #3]
-    strb r1, [r2, #4]
-    lsr  r3, r1, #8
-    strb r3, [r2, #5]
-    lsr  r3, r3, #8
-    strb r3, [r2, #6]
-    lsr  r3, r3, #8
-    strb r3, [r2, #7]
-    bx   lr
-.previous
-
-.section ".text.aeabi.unaligned.write4"
-aeabi_uwrite4:
-    strb r0, [r1]
-    lsr  r2, r0, #8
-    strb r2, [r1, #1]
-    lsr  r2, r2, #8
-    strb r2, [r1, #2]
-    lsr  r2, r2, #8
-    strb r2, [r1, #3]
-    bx   lr
-.previous
-
 .section ".text.aeabi.idiv"
 aeabi_idiv:
     cmp   r1, #0
