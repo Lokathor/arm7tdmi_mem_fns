154e6071333d9938a98992a10d98c1a95d484447
Lokathor <zefria@gmail.com>
2022-07-29 10:12:02 -0600

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,12 @@
+extern crate proc_macro;
+use proc_macro::TokenStream;
+
 /// This holds all the `extern "C"` declarations for the assembly code that the
 /// crate can provide.
 #[warn(missing_docs)]
 pub mod fn_declarations;
+
+#[proc_macro]
+pub fn make_answer(_item: TokenStream) -> TokenStream {
+    "fn answer() -> u32 { 42 }".parse().unwrap()
+}
